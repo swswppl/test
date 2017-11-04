26d1f11061485a5a552242a542a00139a768dee3
swswppl <33370185+swswppl@users.noreply.github.com>
2017-11-04 11:53:06 +0100

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package pk.edu.pk.jp.helloworldd;
+
+/**
+ * @author dell
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
